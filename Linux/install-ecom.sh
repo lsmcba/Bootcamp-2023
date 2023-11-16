2a459cb1fb1e7d40148d3fb1a41661d834d847ee
Leonardo Moya <lsmcba@gmail.com>
2023-11-16 13:33:29 -0300

    Trabajo Linux Ecommerce Finalizado

--- Linux/install-ecom.sh
+++ Linux/install-ecom.sh
@@ -89,7 +89,7 @@ echo -e "${Yellow}Creando archivo SQL y agregarlo a la base ecommerce${Color_off
 echo
 sleep 2
 cat > db-load-script.sql <<-EOF
-USE ecomerce;
+USE ecomdb;
 CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
 INSERT INTO products (Name,Price,ImageUrl) VALUES ("Laptop","100","c-1.png"),("Drone","200","c-2.png"),("VR","300","c-3.png"),("Tablet","50","c-5.png"),("Watch","90","c-6.png"),("Phone Covers","20","c-7.png"),("Phone","80","c-8.png"),("Laptop","150","c-4.png");
 EOF
