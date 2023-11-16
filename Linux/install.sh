c92dc4ef2b80b8bacfe961657f1ff7147e07d3aa
= <=>
2023-11-16 02:03:01 -0300

    Modificacion instalacion correcta

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,119 +1,117 @@
 #!/bin/bash
 
-#check sudo
+Red='\033[0;31m'    
+Green='\033[0;32m'  
+Yellow='\033[0;33m' 
+White='\033[0;97m'
+
 if [ "$(id -u)" -ne 0 ]; then
-    echo -e "\033[33mCorrer con SUDO\033[0m"
+    echo -e "${Red}Error: Ejecutarlo con SUDO..........${Color_off}"
     exit
 fi 
 
-#Update
-echo -e "${Green}Actualizando repositorio de Linux......${Color_Off}"
+echo -e "${Yellow}Actualizando repositorio de Linux......${Color_Off}"
 apt-get update
 
-
-#Install Apache, Git and MariaDB
-#Install required packages
-
-echo "${Green}Verificando si apache esta instalado.....${Color_Off}"
+echo -e "${Yellow}Verificando si apache esta instalado.....${Color_Off}"
 echo
 sleep 2
 if dpkg --get-selections | grep apache2; then 
 	echo
-	echo  "Apache instalado" 
+	echo  -e "${Green}Apache instalado${Color_off}" 
 	apache2ctl -v
 	echo
 else
 	echo
-	echo "Apache no instalado, instalando....."
+	echo -e "${Red}Apache no instalado, instalando.....${Color_off}"
 	echo
 	apt install apache2 php libapache2-mod-php php-mysql -y
     echo
-    echo "Iniciando Servicios apache....."
+    echo -e "${Green}Apache instalado, Iniciando Servicios apache.....${Color_off}"
     systemctl start apache2 
     systemctl enable apache2 
 
 fi
 sleep 3
 
-echo "${Green}Verificando si Git esta instalado.....${Color_Off}"
+echo -e "${Yellow}Verificando si Git esta instalado.....${Color_Off}"
 echo
 sleep 2
 if dpkg --get-selections | grep git; then 
         echo
-        echo  "Git instalado" 
+        echo -e "${Green}Git instalado${Color_off}" 
         git --version
         echo
 else
         echo
-        echo "Apache no instalado, instalando....."
+        echo -e "${Red}Git no instalado, instalando.....${Color_off}"
         echo
         apt install git -y
+        echo
+        echo  -e "${Green}Git instalado${Color_off}" 
+        git --version
+        echo
 
 fi 
 sleep 3
 
-echo "${Green}Verificando si MariaDB esta instalado.....${Color_Off}"
+echo -e "${Yellow}Verificando si MariaDB esta instalado.....${Color_Off}"
 echo
 sleep 2
 if dpkg --get-selections | grep mariadb-server; then 
 	echo
-	echo  "MariaDB instalado" 
+	echo  -e "${Green}MariaDB instalado${Color_off}" 
 	mariadb --version
 	echo
 else
 	echo
-	echo "Apache no instalado, instalando....."
+	echo -e "${Red}Apache no instalado, instalando.....${Color_off}"
 	echo
-	apt install mariadb -y
+	apt install mariadb-server -y
     echo
-    echo "Iniciando Servicios MariaDB....."
+    echo -e "${Green}MariaDB instalado. Iniciando Servicios MariaDB.....${Color_Off}"
     echo
     systemctl start mariadb 
     systemctl enable mariadb 
 fi
 
-#Configure Database
+echo
+echo -e "${Yellow}Instalando la base de datos y usuarios, estableciendo permisos${Color_off}"
+echo
+sleep 2
+mysql -e "CREATE DATABASE ecomerce;
+	      CREATE USER 'lsmcba'@'localhost' IDENTIFIED BY 'contrasenasegura';
+	      GRANT ALL PRIVILEGES ON *.* TO 'lsmcba'@'localhost';
+	      FLUSH PRIVILEGES;"
 
-$ mysql
-MariaDB > CREATE DATABASE ecomdb;
-MariaDB > CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';
-MariaDB > GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'localhost';
-MariaDB > FLUSH PRIVILEGES;
-
-#Agregar datos a la database ecomdb
-#Create the db-load-script.sql
-
+echo
+echo -e "${Yellow}Creando archivo SQL y agregarlo a la base ecommerce${Color_off}"
+echo
+sleep 2
 cat > db-load-script.sql <<-EOF
-USE ecomdb;
+USE ecomerce;
 CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
-
 INSERT INTO products (Name,Price,ImageUrl) VALUES ("Laptop","100","c-1.png"),("Drone","200","c-2.png"),("VR","300","c-3.png"),("Tablet","50","c-5.png"),("Watch","90","c-6.png"),("Phone Covers","20","c-7.png"),("Phone","80","c-8.png"),("Laptop","150","c-4.png");
-
 EOF
-Run sql script
-
-
 mysql < db-load-script.sql
 
-#Deploy and Configure Web
 
-
-#Codigo
-
+echo
+echo -e "${Yellow}Clonando repositorio de Ecommerce${Color_off}"
+echo
 git clone https://github.com/roxsross/The-DevOps-Journey-101.git
 cp -r The-DevOps-Journey-101/CLASE-02/lamp-app-ecommerce/* /var/www/html/
 mv /var/www/html/index.html /var/www/html/index.html.bkp
 
-#Actualizar index.php
-#Update index.php
 
-sudo sed -i 's/172.20.1.101/localhost/g' /var/www/html/index.php
 
-              <?php
-                        $link = mysqli_connect('172.20.1.101', 'ecomuser', 'ecompassword', 'ecomdb');
-                        if ($link) {
-                        $res = mysqli_query($link, "select * from products;");
-                        while ($row = mysqli_fetch_assoc($res)) { ?>
-
-#Test
-curl http://localhost
+echo
+echo -e "${Yellow}Actualizando PHP${Color_off}"
+echo
+sudo sed -i 's/192.168.58.101/localhost/g' /var/www/html/index.php
+    <?php
+        $link = mysqli_connect('192.168.56.101', 'lsmcba', 'contrasenasegura', 'ecomerce');
+        if ($link) {
+        $res = mysqli_query($link, "select * from products;");
+        while ($row = mysqli_fetch_assoc($res)) { 
+    ?>
